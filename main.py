6c0ec6ebb42d77db905a08c345e4fdff1142b0b4
manas ranjan <manas.rj16@gmail.com>
2018-03-23 02:57:32 +0530

    first day work on spychat project in which asking for name,gender,and mobile no from user

---
+++ main.py
@@ -0,0 +1,15 @@
+print 'Hello Buddy! Welcome to the Spy World!'
+print 'let\'s get started'
+spyname = raw_input('What is your good name? ')
+print ('Hey ' + spyname + '. Welcome to the spy world.')
+spy_gender = raw_input('enter your gender. ')
+if spy_gender == "male":
+     print('Hello Mr.' + spyname )
+     print('Here we enter the spy world.')
+else:
+     print('Hello Mrs.' + spyname )
+     print('Here we enter the spy world.')
+print'Alright ' + spyname+ ', I\'d like to know a little bit more about you..'
+mobile_no = input('Enter your spychat Mobile no. ')
+print 'Your spychat Mobile no. is:' + str(mobile_no)
+
